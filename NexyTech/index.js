a758d6294749631bbcee533be25bc80f1b81aa0f
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-05-24 16:27:46 -0300

    css reference section

--- NexyTech/index.js
+++ NexyTech/index.js
@@ -4,9 +4,4 @@ const $navbarLinks = document.querySelector(".navbar__links");
 $menuMobile.addEventListener("click", () => {
     $navbarLinks.classList.toggle("navbar__active");
     $navbarLinks.classList.toggle("wrap-nav");
-})
-
-
-// document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () => {
-//     $navbarLinks.classList.remove("navbar__active");
-// }));
+});
